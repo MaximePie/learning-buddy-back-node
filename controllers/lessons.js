dd650ccd7ce816f3a36ca68efafb4b28e34845a5
Chatmalow <maxime.pie.mail@gmail.com>
2021-04-01 16:34:07 +0200

    feat: First Lesson Created

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,9 +1,39 @@
+const mongoose = require('mongoose');
+const lessonSchema = mongoose.Schema({
+  label: {type: String, required: true},
+  url: {type: String},
+  icon: {type: String},
+});
+
+const Lesson = mongoose.model('Lesson', lessonSchema);
+
+/**
+ * Creates a new Lesson based on the provided data in the request's body
+ * @param request
+ * @param response
+ */
 function create(request, response) {
   if (request.body) {
-    const { title } = request.body;
-    response.status(200).json({message: title});
-  }
-  else {
+    let errors = [];
+    const {label, url, icon} = request.body;
+
+    if (!label) {
+      errors.push('Erreur, il faut un label');
+    }
+
+    if (!errors.length) {
+      Lesson.create({
+        label: label,
+        url: url || '',
+        icon: icon || 'search',
+      }, (error, data) => {
+        response.status(200).json({message: 'La leçon a bien été créée !', data, error});
+      });
+    } else {
+      response.status(400).json({message: errors});
+    }
+
+  } else {
     response.status(404).json({message: 'Aucun body trouvé'});
   }
 }
--- routes.js
+++ routes.js
@@ -5,8 +5,6 @@ const lessonsController = require('./controllers/lessons');
 // Let's put all the routes here.
 // If the amount of routes is too big, create a folder and split the file, one file for each entity should do the work
 
-// const Lesson = mongoose.model('Cat', { name: String });
-
 // Lessons
 router.get('/lessons/', lessonsController.index);
 router.post('/lessons/', lessonsController.create);
